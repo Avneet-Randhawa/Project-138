327c93c91bebcf235de05d7e1b166ef3912be8be
Avneet Randhawa <76783244+Avneet-Randhawa@users.noreply.github.com>
2022-10-25 19:17:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -24,6 +24,16 @@ var ball = {
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent("canvas");
+
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  posenet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded(){
+  console.log("Model Is Loaded!!!");
 }
 
 
